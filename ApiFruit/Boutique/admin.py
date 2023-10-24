1be4d412f90d0ce02f72dbc76091e55751a54d38
MathieuDionne2032450 <2032450@etu.cchic.ca>
2023-10-24 10:35:21 -0400

    Model

--- ApiFruit/Boutique/admin.py
+++ ApiFruit/Boutique/admin.py
@@ -4,6 +4,14 @@ from . import models
 # Register your models here.
 @admin.register(models.Categorie)
 class CategorieAdmin(admin.ModelAdmin):
-    list_display = ['nom','description']
+    list_display = ['nom','description','img']
     ordering = ['nom']
     search_fields = ['nom']
+
+@admin.register(models.Produit)
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ['nom_produit','description_produit','id_categorie','quantite_stock','prix']
+    ordering = ['nom_produit']
+    search_fields = ['nom_produit']
+
+
--- ApiFruit/Boutique/models.py
+++ ApiFruit/Boutique/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -7,3 +9,34 @@ class Categorie(models.Model):
     nom=models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     img = models.CharField(max_length=255)
+
+class Produit(models.Model):
+    nom_produit = models.CharField(max_length=255,null=False)
+    quantite_stock = models.IntegerField(validators=[MinValueValidator(0,"La quantite doit etre superrieur a 0")], default=0, null=False)
+    id_categorie = models.IntegerField(validators=[MinValueValidator(0,"L'id doit etre superrieur a 0")], null=False)
+    description_produit = models.CharField(max_length=255,null=True)
+    prix = models.FloatField(validators=[MinValueValidator(0,"Le prix doit etre superrieur a 0")],null=False, default=0)
+
+class ProduitImg (models.Model):
+    id_produit = models.IntegerField(validators=[MinValueValidator(0,"L'id doit etre superieur a 0")], null=False)
+    path = models.CharField(max_length=255, null=True)
+
+class Promotion(models.Model):
+    type_rabais = models.CharField(max_length=255,null=False)
+    valeur = models.IntegerField(validators=[MinValueValidator(0,"La valeur doit etre superieur a 0")],null=False)
+    description_promotion = models.CharField(max_length=255,null=True)
+
+class Commande(models.Model):
+    #determine si la commande est dans le panier ou si elle est paye et en cours de livraison
+    commander = models.BooleanField(default=False)
+    dateCommander = models.DateTimeField(null=True)
+    id_client = models.IntegerField(validators=[MinValueValidator(0,"L'id doit etre superrieur a 0")],null=False)
+
+class Client(models.Model):
+    nom_client = models.CharField(max_length=255,null=False)
+    prenom_client = models.CharField(max_length=255,null=False)
+    mot_de_passe = models.CharField(max_length=255,null=False)
+    adresse = models.CharField(max_length=255,null=False)
+    courriel = models.CharField(max_length=255,null=False)
+    num_telephone = models.CharField(max_length=255,null=True)
+    date_inscription = models.DateTimeField()
