1ad2313cf7652720b2ac0801813bf0e089d283fa
Alexandre1832686 <1832686@etu.cchic.ca>
2023-10-24 11:59:22 -0400

    Alex 24

--- ApiFruit/Boutique/models.py
+++ ApiFruit/Boutique/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
 #Classe Catégorie
 class Categorie(models.Model):
     nom=models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+    img = models.CharField(max_length=255)
+
+class Produit(models.Model):
+    name = models.CharField(max_length=255)
+    Description = models.CharField(max_length=255)
+    prix = models.FloatField()
     img = models.CharField(max_length=255)
--- ApiFruit/Boutique/views.py
+++ ApiFruit/Boutique/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.template import loader
+from . import models
+from django.http import HttpResponse
 
 # Create your views here.
 def Accueil(request):
- 
-    return render(request,'accueil.html')
+    produits = models.Produit.objects.all()
+    for p in produits:
+        p.delete()
+    prod1 = models.Produit(name="Pomme Honey Crisp",Description="1 fruit (environ 200 g) ",prix=2.99,img="pomme.png")
+    prod1.save()
+    prod1 = models.Produit(name="Poire Barlett",Description="1 fruit (environ 200 g) ",prix=3.99,img="poire.png")
+    prod1.save()
+
+    produits = models.Produit.objects.all()
+
+    context = {
+        'produits':produits,
+    }
+
+    return render(request,'accueil.html',context)
+
+
+
+   
