540dc0d9264ba983465c7e913b38efa98f2a7dc8
Alexandre1832686 <1832686@etu.cchic.ca>
2023-11-07 11:59:11 -0500

    alex

--- ApiFruit/Boutique/urls.py
+++ ApiFruit/Boutique/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Accueil, name='accueil'),
-    path('categorie/<int:id>/', views.Fruits, name='categorie'),
+    path('categorie/<int:id_>/', views.Fruits, name='categorie'),
     path('categories/', views.Categories, name='categories'),
     path('peupler/', views.Peupler, name='peupler'),
 
